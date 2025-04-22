04419b3d9df85889320212a2e453eedf66769c20
OMKARGOPCHADE <omkargopchhade@gmail.com>
2025-04-23 00:24:14 +0530

    Props and Tailwind Injection in react using vite

---
+++ propsApp/components/card.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+function Card({bName,bId,bPrice}) {
+    return (  
+        <div className="flex flex-col rounded-xl  p-4 mt-5"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{bName}</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>{bId}</p>
+            <p>{bPrice}</p>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Card;
