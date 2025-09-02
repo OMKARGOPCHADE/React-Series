5bba80a5205967d768f4063002ee5d996ec75e4c
Omkar Gopchade <86941500+OMKARGOPCHADE@users.noreply.github.com>
2025-09-02 19:47:18 +0530

    Update db.js

--- my-schools-app/utils/db.js
+++ my-schools-app/utils/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",       // change if needed
-  password: "sharvi@123",       // your MySQL password
+  password: "*******",       // your MySQL password
   database: "schooldb",
 });
 
